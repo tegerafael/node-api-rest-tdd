4118f3409116f2bd4a59efe44bb6755e612e4c7e
Tiago Rafael Oliveira das Virgens <tiago.rafael@compasso.com.br>
2023-12-05 12:08:00 -0400

    feat: create user

--- src/app.js
+++ src/app.js
@@ -1,7 +1,21 @@
 const app = require('express')();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
     res.status(200).send();
+});
+
+app.get('/users', (req, res) => {
+    const users = [
+        { name: 'John Doe', email: 'john@gmail.com' }
+    ];
+    res.status(200).json(users);
+});
+
+app.post('/users', (req, res) => {
+    res.status(201).json(req.body);
 });
 
 module.exports = app;
